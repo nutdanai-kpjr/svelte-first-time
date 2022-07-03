5b885b6c72c968c7a0bf1e2e07a2db87d27ec013
nutdanai-kpjr <nutdanai.282@mail.kmutt.ac.th>
2022-07-03 19:12:40 +0700

    Update about section detail

--- portfolio/src/lib/components/AboutSection.svelte
+++ portfolio/src/lib/components/AboutSection.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-    import PrimarySection from '$lib/components/layout/PrimarySection.svelte';
+	import PrimarySection from '$lib/components/layout/PrimarySection.svelte';
 </script>
 
-<PrimarySection><h1>About me</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A cupiditate, dolorum commodi accusantium tenetur perferendis necessitatibus veritatis ratione officia maiores ea placeat quo minima mollitia nesciunt quas sapiente assumenda. Saepe.</p>
-<a>View My CV</a>
+<PrimarySection
+	><h1>About me</h1>
+	<p>
+		My name is Nutdanai Kitphermjaroen. I am CS student who interested in using web and mobile
+		applications to improve people's well-being.
+	</p>
+	<p>
+		As an undergraduate student studying computer science, I have developed web and mobile
+		applications both for my personal interests and for university projects since 2019.
+	</p>
+	<a>View My CV</a>
 </PrimarySection>
 
 <style>
-
 </style>
